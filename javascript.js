45921b66a3a926f06944ff1c3d10f568720c16b7
cacheup <dcastelan613@gmail.com>
2022-04-09 07:45:30 -0400

    Add function that gets a random move

---
+++ javascript.js
@@ -0,0 +1,13 @@
+function computerPlay() {
+  let choice = Math.floor(Math.random()*3) + 1;
+  switch (choice) {
+    case 1:
+    return "rock";
+
+    case 2:
+    return "paper";
+
+    case 3:
+    return "scissors";
+  }
+}
