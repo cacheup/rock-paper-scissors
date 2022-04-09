b6456e6c17400c4b8be3b242094fc95a765cbce2
cacheup <dcastelan613@gmail.com>
2022-04-09 08:48:36 -0400

    Add function that gets the result of a round

--- javascript.js
+++ javascript.js
@@ -11,3 +11,39 @@ function computerPlay() {
     return "scissors";
   }
 }
+//gets result of a round; "win", "tie", or "lose"
+function playRound(playerSelection, computerSelection) {
+  if(playerSelection == "rock") {
+    if(computerSelection == "rock") {
+      return "tie";
+    } 
+    else if(computerSelection == "paper") {
+      return "lose";
+    } 
+    else if(computerSelection == "scissors") {
+      return "win";
+    }
+  } 
+  else if(playerSelection == "paper") {
+    if(computerSelection == "rock") {
+        return "win";
+    } 
+    else if(computerSelection == "paper") {
+      return "tie";
+    } 
+    else if(computerSelection == "scissors") {
+      return "lose";
+    }
+  }
+  else if (playerSelection == "scissors") {
+    if(computerSelection == "rock") {
+      return "lose";
+    }
+    else if(computerSelection == "paper") {
+      return "win";
+    }
+    else if(computerSelection == "scissors") {
+      return "tie";
+    }
+  }
+}
